4b8317254895379b6361f07c6515fd56d1a9d7c0
Ravi Ranjan Singh <56018516+rvi077344@users.noreply.github.com>
2022-12-02 16:55:41 +0530

    Add files via upload

---
+++ Calculator.ps1
@@ -0,0 +1,33 @@
+﻿[int]$x = Read-host -Prompt "Enter first number"
+[char]$y = Read-host -Prompt "Enter Operation"
+[int]$z = Read-host -Prompt "Enter second number"
+
+[int]$add = $x+$z
+[int]$subtraction = $x-$z
+[int]$multiply = $x*$z
+[double]$divide = $x/$z
+[int]$modulus = $x%$z
+
+switch($y)
+{
+  +{$result = "Addtion"
+     Write-Host("Your answer is $add")
+     break;
+   }
+  -{$result = "Subtraction"
+     Write-Host("Your answer is $subtraction")
+     break;
+   }
+  *{$result = "Multiply"
+     Write-Host("Your answer is $multiply")
+     break;
+   }
+  /{$result = "Divide"
+     Write-Host("Your answer is $divide")
+     break;
+   }
+  %{$result = "Modulus"
+     Write-Host("Your answer is $modulus")
+     break;
+   }
+}
