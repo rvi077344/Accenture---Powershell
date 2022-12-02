643d4a5da99cffc0e62bd97f39427d1572b1343e
Ravi Ranjan Singh <56018516+rvi077344@users.noreply.github.com>
2022-12-02 16:52:20 +0530

    Add files via upload

---
+++ Factorial.ps1
@@ -0,0 +1,20 @@
+﻿function Get-Factorial([int]$value)
+{
+    if($value -lt 0)
+    {
+        $result = 0
+    }
+    elseif($value -le 1)
+    {
+        $result = 1
+    }
+    else
+    {
+        $result = $value * (Get-Factorial($value - 1))
+    }
+    return $result
+}
+
+$value = Read-Host 'Enter a value'
+$result = Get-Factorial $value
+Write-Output "$value! = $result"
